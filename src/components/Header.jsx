778d037e0948dc3fe549482cd82d332bbf33f4b7
Haha200188 <97596109+Haha200188@users.noreply.github.com>
2023-08-13 20:12:45 +0800

    homepage trending product

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,29 +3,40 @@ import '../styles/Header.scss'
 import './SearchModal'
 import { Navbar, Nav } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping, faUser, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import {
+  faCartShopping,
+  faUser,
+  faMagnifyingGlass,
+} from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
 import SearchModal from './SearchModal'
-
-
 
 const Header = () => {
   const [showModal, setShowModal] = useState(false)
   const handleClose = () => setShowModal(false)
   const handleShow = () => setShowModal(true)
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand as={Link} to="/">LOGO</Navbar.Brand>
+    <Navbar className="clearPadding" bg="light" expand="lg">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse className="header" id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link as={Link} to="/">
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to="/shop">
-            Shop
-          </Nav.Link>
-        </Nav>
+      <Navbar.Collapse className="headerContainer" id="basic-navbar-nav">
+        <div className="header">
+          <Navbar.Brand className="headerLogo" as={Link} to="/" />
+          THE ART OF ET
+          <Nav className="ml-auto headerPage">
+            <Nav.Link as={Link} to="/">
+              home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/shop">
+              shop
+            </Nav.Link>
+            <Nav.Link as={Link} to="/myWork">
+              my work
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact">
+              contact
+            </Nav.Link>
+          </Nav>
+        </div>
         <Nav className="ml-auto headerIcon">
           <Nav.Link>
             <FontAwesomeIcon icon={faCartShopping} />
@@ -34,7 +45,7 @@ const Header = () => {
             <FontAwesomeIcon icon={faUser} />
           </Nav.Link>
           <Nav.Link>
-            <FontAwesomeIcon icon={faMagnifyingGlass} onClick={handleShow}/>
+            <FontAwesomeIcon icon={faMagnifyingGlass} onClick={handleShow} />
             {showModal && <div className="overlay" onClick={handleClose} />}
             <SearchModal show={showModal} handleClose={handleClose} />
           </Nav.Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import { Container } from 'react-bootstrap'
 import '../styles/Home.scss'
 
 const Home = () => {
   return (
     <div className='homeContainer'>
       <Header />
-      <Container className="my-5">
-        <h1>Welcome to My Website</h1>
-        <p>This is a simple example of a React and Bootstrap 5 website.</p>
-      </Container>
+      <div className="homePage">
+        <div className='mainImg' />
+        <div className='homeTitle'>These are people's favorites</div>
+        <div className="trendingContainer">
+          <div className="trendingProduct" />
+          <div className="trendingProduct" />
+          <div className="trendingProduct" />
+          <div className="trendingProduct" />
+          <div className="trendingProduct" />
+        </div>
+        <div className='homeTitle'>The bliss we once tailored</div>
+      </div>
       <Footer />
     </div>
   )
