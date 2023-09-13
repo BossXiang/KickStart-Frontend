16efb8e071a95726a5a17bd6f596fd9eb14f3d08
Haha200188 <97596109+Haha200188@users.noreply.github.com>
2023-09-13 23:03:28 +0800

    search order页面切板

--- src/pages/SearchOrder.jsx
+++ src/pages/SearchOrder.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import '../styles/SearchOrder.scss'
+import { Form, Button } from 'react-bootstrap'
+import { searchOrder } from '../plugins/api/api_service.ts'
 
 const SearchOrder = () => {
+  const [searchText, setSearchText] = useState('')
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value)
+  }
+
+  const [searchResult, setSearchResult] = useState(null)
+
+  const handleSearchClick = async () => {
+    try {
+      const result = await searchOrder(searchText)
+      setSearchResult(result)
+      console.log(searchResult)
+    } catch (error) {
+      console.error('Error searching order:', error)
+    }
+  }
   return (
-    <div className='searchContainer'>
+    <div className="searchContainer">
       <Header />
-      <div className="searchPage"></div>
+      <div className="searchPage">
+        <div className="searchContent">
+          <div className="searchTitle">Order Query</div>
+
+          <Form className="inputGroup">
+            <div id="searchInput">
+              <Form.Control
+                type="text"
+                placeholder="Enter order number"
+                value={searchText}
+                onChange={handleSearchChange}
+              />
+              <div className="example">For example: 123456789</div>
+            </div>
+            <Button
+              id="searchBtn"
+              variant="primary"
+              onClick={handleSearchClick}>
+              Search
+            </Button>
+          </Form>
+        </div>
+      </div>
       <Footer />
     </div>
   )
