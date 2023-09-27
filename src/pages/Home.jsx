c053bac9ecf0cbdc399fc268853ceccdf09195cc
Haha200188 <97596109+Haha200188@users.noreply.github.com>
2023-09-28 00:22:17 +0800

    home文字

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     'assets/img/hot1.png',
     'assets/img/hot2.png',
     'assets/img/hot3.png',
-    'assets/img/hot4.png'
+    'assets/img/hot4.png',
   ]
   const [isVisible1, setIsVisible1] = useState(false)
   const [isVisible2, setIsVisible2] = useState(false)
@@ -22,22 +22,22 @@ const Home = () => {
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 200) {
-        setIsVisible1(true);
+        setIsVisible1(true)
         setTimeout(() => {
-          setIsVisible2(true);
-        }, 100);
+          setIsVisible2(true)
+        }, 100)
         setTimeout(() => {
-          setIsVisible3(true);
-        }, 200);
+          setIsVisible3(true)
+        }, 200)
       }
       if (window.scrollY > 600) {
-        setIsVisible4(true);
+        setIsVisible4(true)
         setTimeout(() => {
-          setIsVisible5(true);
-        }, 100);
+          setIsVisible5(true)
+        }, 100)
         setTimeout(() => {
-          setIsVisible6(true);
-        }, 200);
+          setIsVisible6(true)
+        }, 200)
       }
     }
     window.addEventListener('scroll', handleScroll)
@@ -64,34 +64,72 @@ const Home = () => {
           className="mainImg"
         />
         <div className="homeTitle">The bliss we once tailored</div>
-        <div className="homeDescription">Elevate your photo into a captivating fantasy world,<br/>offering a plethora of sophisticated stylistic choices!</div>
         <div className="workContainer">
           <div className="work">
             <div className="pic">
               <img src="assets/img/blonde.png" alt="pic" />
             </div>
             <div className="paintGroup">
-              {isVisible1 && <div className="paint"><img src="assets/img/blonde1.png" alt="pic" /></div>}
-              {isVisible2 && <div className="paint"><img src="assets/img/blonde2.png" alt="pic" /></div>}
-              {isVisible3 && <div className="paint"><img src="assets/img/blonde3.png" alt="pic" /></div>}
+              {isVisible1 && (
+                <div className="paint">
+                  <img src="assets/img/blonde1.png" alt="pic" />
+                </div>
+              )}
+              {isVisible2 && (
+                <div className="paint">
+                  <img src="assets/img/blonde2.png" alt="pic" />
+                </div>
+              )}
+              {isVisible3 && (
+                <div className="paint">
+                  <img src="assets/img/blonde3.png" alt="pic" />
+                </div>
+              )}
             </div>
           </div>
           <div className="work">
             <div className="paintGroup">
-              {isVisible6 && <div className="paint"><img src="assets/img/dog1.jpg" alt="pic" /></div>}
-              {isVisible5 && <div className="paint"><img src="assets/img/dog2.jpg" alt="pic" /></div>}
-              {isVisible4 && <div className="paint"><img src="assets/img/dog3.jpg" alt="pic" /></div>}
+              {isVisible6 && (
+                <div className="paint">
+                  <img src="assets/img/dog1.jpg" alt="pic" />
+                </div>
+              )}
+              {isVisible5 && (
+                <div className="paint">
+                  <img src="assets/img/dog2.jpg" alt="pic" />
+                </div>
+              )}
+              {isVisible4 && (
+                <div className="paint">
+                  <img src="assets/img/dog3.jpg" alt="pic" />
+                </div>
+              )}
             </div>
             <div className="pic">
               <img src="assets/img/dog.png" alt="pic" />
             </div>
           </div>
         </div>
+        <div className="homeText">
+          <div className="star star1"></div>
+          <div className="star star2"></div>
+          <div className="star star3"></div>
+          <div className="star star4"></div>
+          <div className="star star5"></div>
+          <div className="star star6"></div>
+          <div className="star star7"></div>
+          <div className="star star8"></div>
+          <div className="star star9"></div>
+          <div className="textContent">
+            'Elevate your photo into a captivating fantasy world,
+            <br />
+            offering a plethora of sophisticated stylistic choices!'
+          </div>
+        </div>
         <div className="homeTitle">These are people's favorites</div>
         <div className="trendingContainer">
           <Slider images={images} />
         </div>
-        <div className="homeText">Saying something</div>
         <div className="bottomPic">
           <img src="assets/img/bottom.png" alt="bottom" />
         </div>
